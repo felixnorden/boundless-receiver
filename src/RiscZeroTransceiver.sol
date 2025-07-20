621ef905729bd2320b65fb09546c0fc03e510505
Felix Nordén <felixnorden@gmail.com>
2025-07-20 20:51:36 +0100

    feat: add transition function admin functions, test admin functions

--- src/RiscZeroTransceiver.sol
+++ src/RiscZeroTransceiver.sol
@@ -1,13 +1,101 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.30;
 
-import { ConsensusState } from "./tseth.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { Steel } from "@risc0/contracts/steel/Steel.sol";
+import { IRiscZeroVerifier } from "@risc0/contracts/IRiscZeroVerifier.sol";
+import { ConsensusState, Checkpoint } from "./tseth.sol";
 
-contract RiscZeroTransceiver {
+contract RiscZeroTransceiver is AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    bytes32 public imageID;
+    address public immutable verifier;
+    uint24 public permissibleTimespan;
+
+    ConsensusState private currentState;
 
     struct Journal {
         ConsensusState preState;
         ConsensusState postState;
+    }
+
+    event Transition(bytes32 preRoot, bytes32 indexed postRoot, ConsensusState preState, ConsensusState postState);
+
+    error InvalidArgument();
+    error InvalidPreState();
+    error PermissibleTimespanLapsed();
+
+    constructor(
+        ConsensusState memory startingState,
+        uint24 permissibleTimespan_,
+        address verifier_,
+        bytes32 imageID_,
+        address admin,
+        address roleAdmin
+    ) {
+        _grantRole(ADMIN_ROLE, admin);
+        _grantRole(DEFAULT_ADMIN_ROLE, roleAdmin);
+
+        currentState = startingState;
+        permissibleTimespan = permissibleTimespan_;
+        verifier = verifier_;
+        imageID = imageID_;
+    }
+
+    function transition(bytes calldata journalData, bytes calldata seal) external {
+        Journal memory journal = abi.decode(journalData, (Journal));
+        if (!_compareConsensusState(currentState, journal.preState)) {
+            revert InvalidPreState();
+        }
+        if (!_permissibleTransition(journal.preState, journal.postState)) {
+            revert PermissibleTimespanLapsed();
+        }
+
+        bytes32 journalHash = sha256(journalData);
+        IRiscZeroVerifier(verifier).verify(seal, imageID, journalHash);
+
+        currentState = journal.postState;
+        emit Transition(
+            journal.preState.currentJustifiedCheckpoint.root,
+            journal.postState.currentJustifiedCheckpoint.root,
+            journal.preState,
+            journal.postState
+        );
+    }
+
+    function checkpoint() external view returns (Checkpoint memory current) {
+        current = currentState.currentJustifiedCheckpoint;
+    }
+
+    function updateImageID(bytes32 newImageID) external onlyRole(ADMIN_ROLE) {
+        if (newImageID == imageID) revert InvalidArgument();
+        imageID = newImageID;
+    }
+
+    function updatePermissibleTimespan(uint24 newPermissibleTimespan) external onlyRole(ADMIN_ROLE) {
+        if (newPermissibleTimespan == permissibleTimespan) revert InvalidArgument();
+        permissibleTimespan = newPermissibleTimespan;
+    }
+
+    function _compareConsensusState(ConsensusState memory a, ConsensusState memory b) internal pure returns (bool) {
+        return _compareCheckpoint(a.currentJustifiedCheckpoint, b.currentJustifiedCheckpoint)
+            && _compareCheckpoint(a.finalizedCheckpoint, b.finalizedCheckpoint);
+    }
+
+    function _compareCheckpoint(Checkpoint memory a, Checkpoint memory b) internal pure returns (bool) {
+        return a.epoch == b.epoch && a.root == b.root;
+    }
+
+    function _permissibleTransition(
+        ConsensusState memory pre,
+        ConsensusState memory post
+    )
+        internal
+        view
+        returns (bool)
+    {
+        uint256 transitionTimespan = post.currentJustifiedCheckpoint.epoch - pre.currentJustifiedCheckpoint.epoch;
+        return transitionTimespan <= uint256(permissibleTimespan);
     }
 }
---
+++ test/RiscZeroTransceiver.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.30;
+
+import { IRiscZeroVerifier } from "@risc0/contracts/IRiscZeroVerifier.sol";
+import { ConsensusState, Checkpoint } from "../src/tseth.sol";
+import { RiscZeroTransceiver } from "../src/RiscZeroTransceiver.sol";
+
+import { Test } from "forge-std/Test.sol";
+import { console2 } from "forge-std/console2.sol";
+
+struct Proof {
+    uint256 epoch;
+    bytes journal;
+    bytes seal;
+    bytes request_id;
+    uint256 block_number;
+    uint256 block_timestamp;
+    bytes32 fulfillment_tx_hash;
+    uint256 fulfillment_chain_id;
+    uint256 created_at;
+    bytes32 image_id;
+    bytes receipt;
+}
+
+contract RiscZeroTransceiverTest is Test {
+    RiscZeroTransceiver rzt;
+    ConsensusState root;
+    bytes32 imageID;
+    address verifier;
+    uint24 permissibleTimespan;
+    address admin;
+    address user;
+
+    function setUp() public {
+        string memory proofData = vm.readFile("./test/proof.json");
+
+        RiscZeroTransceiver.Journal memory journal =
+            abi.decode(vm.parseJsonBytes(proofData, ".journal"), (RiscZeroTransceiver.Journal));
+        root = journal.postState;
+        imageID = vm.parseJsonBytes32(proofData, ".image_id");
+        permissibleTimespan = 3600 * 24 * 3; // 72 hr timespan
+        verifier = address(0x1337);
+        admin = address(0xA11CE);
+        user = address(0xB0B);
+
+        rzt = new RiscZeroTransceiver(root, permissibleTimespan, verifier, imageID, admin, admin);
+    }
+
+    function test_UserHasNoAdminRole() public view {
+        assertFalse(rzt.hasRole(rzt.ADMIN_ROLE(), user));
+    }
+
+    function test_AdminHasAdminRole() public view {
+        assertTrue(rzt.hasRole(rzt.ADMIN_ROLE(), admin));
+    }
+
+    function test_AdminHasDefaultAdminRole() public view {
+        assertTrue(rzt.hasRole(rzt.DEFAULT_ADMIN_ROLE(), admin));
+    }
+
+    function testFuzz_AdminCanUpdatePermissibleTimespan(uint24 newPermissibleTimespan) public {
+        vm.prank(admin);
+        if (newPermissibleTimespan == permissibleTimespan) {
+            vm.expectRevert(RiscZeroTransceiver.InvalidArgument.selector);
+        }
+        rzt.updatePermissibleTimespan(newPermissibleTimespan);
+
+        if (newPermissibleTimespan != permissibleTimespan) {
+            vm.assertEq(rzt.permissibleTimespan(), newPermissibleTimespan);
+        }
+    }
+
+    function testFuzz_AdminCanUpdateImageID(bytes32 newImageID) public {
+        vm.prank(admin);
+        if (newImageID == imageID) {
+            vm.expectRevert(RiscZeroTransceiver.InvalidArgument.selector);
+        }
+        rzt.updateImageID(newImageID);
+
+        if (newImageID != imageID) {
+            vm.assertEq(rzt.imageID(), newImageID);
+        }
+    }
+}
